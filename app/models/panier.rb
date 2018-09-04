61c1e6866794b36eec37c318c471576a176e78fb
FloLeh <florianlehuede@gmail.com>
2018-09-04 20:44:51 +0200

    fix bis panier_plats

--- app/models/panier.rb
+++ app/models/panier.rb
@@ -1,3 +1,4 @@
 class Panier < ApplicationRecord
     has_and_belongs_to_many :plats
+    has_many :panier_plats
 end
--- app/models/plat.rb
+++ app/models/plat.rb
@@ -1,4 +1,5 @@
 class Plat < ApplicationRecord
     belongs_to :user
     has_and_belongs_to_many :paniers
+    has_many :panier_plats
 end
