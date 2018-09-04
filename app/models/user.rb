652f7316cddf63894a8d382c4bb79139ef2d732b
FloLeh <florianlehuede@gmail.com>
2018-09-04 13:15:20 +0200

    drop post + restaurant views

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :téléphone, :presence => {:message => 'champ obligatoire'},
+                        :numericality => {:message => 'doit être un nombre'},
+                        :length => { :minimum => 10, :maximum => 10 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_04_084429) do
+ActiveRecord::Schema.define(version: 2018_09_04_111406) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.string "description"
-    t.integer "prix"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "restaurants", force: :cascade do |t|
     t.string "nom"
