dea2626da3b076e251b48cf1c16f9db22f9f1895
FloLeh <florianlehuede@gmail.com>
2018-09-04 10:59:33 +0200

    - post

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :restaurants
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  resources :posts
   root to: 'home#index'
   devise_for :users
 end
