83726ef5bbb78e6e4afe50f668eef5ab9c8f1002
FloLeh <florianlehuede@gmail.com>
2018-09-06 19:39:38 +0200

    Order front + seed complet

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root to: 'home#index'
   devise_for :users
+  get '/commande', to: "order#show", as: "order"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+PaniersPlat.destroy_all
 Plat.destroy_all
-
+example = User.find_by(email: "test@example.com").id
+Order.destroy(Order.find_by(user_id: example).id)
+Panier.destroy(Panier.find_by(user_id: example).id)
+User.destroy(example)
 u = User.create(prénom: "Crash", nom: "Test", ville: "Nantes", téléphone: "0606060606", email: "test@example.com", password: "foobar", password_confirmation: "foobar")
-
+pan = Panier.create(user_id: u.id)
 4.times do |i|
 
-  Plat.create(user_id: u.id, titre: "plat#{i}", description: "voilà mon plat", prix: 9, stock: 1, plage_horaire: "8h-20h", image_url: "plat#{i}")
+  plat = Plat.create(user_id: u.id, titre: "plat#{i}", description: "voilà mon plat", prix: 9, stock: 1, plage_horaire: "8h-20h", image_url: "plat#{i}")
+  PaniersPlat.create(panier_id: pan.id, plat_id: plat.id, quantité: 1)
   i += 1;
 
 end
+Order.create(user_id: u.id, panier_id: pan.id)
