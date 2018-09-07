97a8df2b9efd0c7b42eadc48755564a1c21dd0af
you@example.com <bertynboulikou@gmail.com>
2018-09-07 17:08:09 +0200

    ajout du edit et index  plat

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root to: 'home#index'
   devise_for :users
-  get '/plats/:id', to: 'plats#show', as:"plats"
+  
   get '/commande', to: "order#show", as: "order"
   get '/paniers', to: 'paniers#show', as: "paniers"
   post '/paniers', to: 'home#ajout_panier'
+  resources :plats
 end
