cf8b1a2d12272b297e6f7177b248fef2722a9ffa
FloLeh <florianlehuede@gmail.com>
2018-09-12 20:32:06 +0200

    some things

--- app/models/plat.rb
+++ app/models/plat.rb
@@ -1,5 +1,6 @@
 class Plat < ApplicationRecord
-    belongs_to :user
+    belongs_to :user, optional: true
+    belongs_to :restaurant, optional: true
     has_and_belongs_to_many :paniers
     has_many :paniers_plats
     has_one_attached :photo_plat
