b4de4662fb4a940b7f37871ee40292effc3b251d
FloLeh <florianlehuede@gmail.com>
2018-09-12 21:53:37 +0200

    seed complet + fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,8 +11,12 @@ PaniersPlat.destroy_all
 Plat.destroy_all
 if User.find_by(email: "test@example.com")
   example = User.find_by(email: "test@example.com").id
-  Order.destroy(Order.find_by(user_id: example).id)
-  Panier.destroy(Panier.find_by(user_id: example).id)
+  if Order.find_by(user_id: example)
+    Order.destroy(Order.find_by(user_id: example).id)
+  end
+  if Panier.find_by(user_id: example)
+    Panier.destroy(Panier.find_by(user_id: example).id)
+  end
   User.destroy(example)
 end
 u = User.create(
@@ -29,15 +33,33 @@ u.image_profile.attach(
   io: File.open("app/assets/images/avatar.jpg"),
   filename: "avatar.jpg"
 )
+
+if Restaurant.find_by(email: "resto@example.com")
+  resto = Restaurant.find_by(email: "resto@example.com").id
+  Restaurant.destroy(resto)
+end
+
+r = Restaurant.create(
+  nom: "Resto-Test",
+  adresse: "2 rue de la Biscuiterie, 44000 Nantes",
+  téléphone: "0635214578",
+  email: "resto@example.com",
+  password: "foobar",
+  password_confirmation: "foobar"
+)
+r.image_resto.attach(
+  io: File.open("app/assets/images/avatar-resto.png"),
+  filename: "avatar-resto.png"
+)
 pan = Panier.create(user_id: u.id)
 7.times do |i|
 
   plat = Plat.create(user_id: u.id,
     titre: Faker::Food.dish,
     description: Faker::Food.description,
-    prix: 9,
-    stock: 5,
-    plage_horaire: "8h-20h"
+    prix: rand(1..5),
+    stock: rand(1..10),
+    plage_horaire: "#{rand(8..12)}h-#{rand(14..22)}h"
   )
   plat.photo_plat.attach(
     io: File.open("app/assets/images/plat#{i}.jpeg"),
