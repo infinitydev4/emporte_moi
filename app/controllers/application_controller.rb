81390648be58f5dd0e6ad70b228a337e0f47ae2b
FloLeh <florianlehuede@gmail.com>
2018-09-14 15:16:55 +0200

    Profils resto et particulier

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :store_return_to, unless: :devise_controller?
 
+  def store_return_to
+   store_location_for(:user, request.url)
+  end
   protected
 
   def configure_permitted_parameters
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_13_184321) do
+ActiveRecord::Schema.define(version: 2018_09_14_123612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,11 +53,11 @@ ActiveRecord::Schema.define(version: 2018_09_13_184321) do
 
   create_table "comments", force: :cascade do |t|
     t.bigint "user_id"
-    t.string "titre"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "restaurant_id"
+    t.integer "auteur_id"
     t.index ["restaurant_id"], name: "index_comments_on_restaurant_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
@@ -157,6 +157,7 @@ ActiveRecord::Schema.define(version: 2018_09_13_184321) do
     t.string "nom"
     t.string "ville"
     t.string "téléphone"
+    t.string "adresse"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
